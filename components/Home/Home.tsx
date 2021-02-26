bf4b7ce33f7602f2207ff23b38b88b709d27d498
Shareef Dweikat <sdweikat@safarway.com>
2021-02-26 15:11:15 +0200

    firebase realtime db installed

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -2,10 +2,18 @@ import React, { Component, useEffect } from "react";
 import { Label } from "./styled";
 import Colors from "../../constants/Colors";
 import { View } from 'react-native'
-import { firebase } from "@react-native-firebase/auth";
+import database from '@react-native-firebase/database';
+
 // import {useSelector} from 'react-redux'
 export default () => {
-  
+        useEffect(()=> {
+            database()
+            .ref('/test/')
+            .once('value')
+            .then(snapshot => {
+              console.log('User data: ', snapshot.val());
+            });
+        }, [])
     // const x  = useSelector((state)=>state.auth)
     // console.log(x)
     return (
